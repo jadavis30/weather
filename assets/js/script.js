60f8afd454dab0946d7cbe4590ee6f7b979641a3
Jared Davis <jadavis@southsidecharter.org>
2020-11-22 12:21:57 -0500

    searched city function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-var searchHistory = $(.search-history)
-fetch()
+// function to search for a city
+function findCity() {
+    var searchTerm = document.getElementById("cityName").value;
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm +"&appid=221098603a041dd7e688a833d2e946ac")
+    .then(function(weatherResponse) {
+        return weatherResponse.json();
+    })
+    .then(function(weatherResponse) {
+        console.log(weatherResponse);
+        var searchedCity = weatherResponse.name;
+        //display at top of current city card
+        var cityWeatherEl = document.querySelector("#searched-city");
+        cityWeatherEl.innerHTML = "<h2>" + searchedCity + "</h2>";
+         
+    });
+}
 
-//
+//appending data to Current Weather Card
+//for symbol: weather.0.icon:
+//for temp main.temp:
+//for hum: main.humidity:
+//for wind speed: main.wind:
