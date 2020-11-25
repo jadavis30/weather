e6836c03ea0f7e60f272b1e2d8e6ed017193a10d
Jared Davis <jadavis@southsidecharter.org>
2020-11-25 00:04:28 -0500

    fixed 5-day forecast formatting and looping

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,43 +27,6 @@ function getCoord(cityName) {
     })
 }
 
-
-// function to search for a city
-function findCity() {
-    
-    var searchTerm = document.getElementById("cityName").value;
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial" + "&appid=221098603a041dd7e688a833d2e946ac")
-    .then(function(weatherResponse) {
-        return weatherResponse.json();
-    })
-    .then(function(weatherResponse) {
-        var namedCity = weatherResponse.name;
-        var weatherGraphic = weatherResponse.weather[0].icon;
-        var namedTemp = weatherResponse.main.temp;
-        var namedHumidity = weatherResponse.main.humidity;
-        var namedWindSpeed = weatherResponse.wind.speed;
-        //display items for current city
-        cityNameEl.innerHTML = "<h2>" + namedCity + "<img src= http://openweathermap.org/img/wn/" + weatherGraphic + ".png>" + "</h2>";
-        cityTempEl.innerHTML = "<p>" + "Temperature: " + namedTemp + "&#176;F" + "</p>";
-        cityHumidityEl.innerHTML = "<p>" + "Humidity: " + namedHumidity + "%" + "</p>";
-        cityWindSpeedEl.innerHTML = "<p>" + "Wind Speed: " + namedWindSpeed + " MPH" + "</p>";
-        
-
-        var cityLat = weatherResponse.coord.lat;
-        var cityLon = weatherResponse.coord.lon;
-        return fetch("http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=221098603a041dd7e688a833d2e946ac");
-        
-    })
-    .then(function(uvResponse) {
-        return uvResponse.json();
-    })
-    .then(function(uvResponse) {
-        var uvIndex = uvResponse.value;
-        cityUvIndexEl.innerHTML = "<p>" + "UV Index: " + uvIndex + "</p>"; 
-    })
-    
-};
-
 //setting searched cities to localstorage
 var list = JSON.parse(localStorage.getItem("prevCities")) || [];
 
@@ -108,17 +71,66 @@ function forecastData() {
         return coordinates;
     })
     .then(function(coordinates) {
-        fetch("https://api.openweathermap.org/data/2.5/onecall?" + "&lat=" + coordinates.latitude + "&lon=" + coordinates.longitude + "&exclude=minutely,hourly,alerts&appid=" + appId)
+        fetch("https://api.openweathermap.org/data/2.5/onecall?" + "&lat=" + coordinates.latitude + "&lon=" + coordinates.longitude + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + appId)
         .then(function(response) {
             return response.json();
         })
         .then(function(responseJson) {
-            console.log(responseJson);
+            var fiveDayForecast = responseJson.daily.slice(1,6); 
+            for (var i = 0; i < fiveDayForecast.length; i++) {
+                renderFutureForecast(fiveDayForecast[i]);
+            }
+            renderCurrentForecast(searchTerm, responseJson.current);
         })    
-        
+    
     })
    
     
+}
+
+function renderCurrentForecast(namedCity, currentForecast) {
+    var weatherGraphic = currentForecast.weather[0].icon;
+    var namedTemp = currentForecast.temp;
+    var namedHumidity = currentForecast.humidity;
+    var namedWindSpeed = currentForecast.wind_speed;
+    var uvIndex = currentForecast.uvi;
+        
+    //display items for current city
+    cityNameEl.innerHTML = "<h2>" + namedCity + getWeatherIconImg(weatherGraphic) + "</h2>";
+    cityTempEl.innerHTML = "<p>" + "Temperature: " + namedTemp + "&#176;F" + "</p>";
+    cityHumidityEl.innerHTML = "<p>" + "Humidity: " + namedHumidity + "%" + "</p>";
+    cityWindSpeedEl.innerHTML = "<p>" + "Wind Speed: " + namedWindSpeed + " MPH" + "</p>";
+    cityUvIndexEl.innerHTML = "<p>" + "UV Index: " + uvIndex + "</p>";
+}
+
+function renderFutureForecast(futureForecast) {
+    var cardDivEl = document.createElement("div");
+    cardDivEl.classList.add("card", "text-white", "bg-info");
+    var divEl = document.createElement("div");
+    divEl.classList.add("card-body");
+    var dateDivEl = document.createElement("div");
+    dateDivEl.classList.add("card-title");
+    dateDivEl.innerText = convertUnixTime(futureForecast.dt);
+    divEl.appendChild(dateDivEl);
+    var paraEl1 = document.createElement("p");
+    paraEl1.classList.add("card-text");
+    paraEl1.innerHTML = getWeatherIconImg(futureForecast.weather[0].icon);
+    divEl.appendChild(paraEl1);
+    var paraEl2 = document.createElement("p");
+    paraEl2.classList.add("card-text");
+    paraEl2.innerHTML = "Temp: " + futureForecast.temp.day + "&#176;F";
+    divEl.appendChild(paraEl2);
+    var paraEl3 = document.createElement("p");
+    paraEl3.classList.add("card-text");
+    paraEl3.innerText = "Humidity: " + futureForecast.humidity + "%";
+    divEl.appendChild(paraEl3);
+    var dailyForecastEl = document.getElementById("daily-forecast");
+    cardDivEl.appendChild(divEl);
+    dailyForecastEl.appendChild(cardDivEl);
+}
+
+function getWeatherIconImg(name) {
+    return "<img src= http://openweathermap.org/img/wn/" + name + ".png>"
 }
 //converting universal time into usable JavaScript time, multiply by 1000 to get miliseconds for needed date
 function convertUnixTime(timestamp) {
