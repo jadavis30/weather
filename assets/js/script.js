0ccd2befa874b6a92a7ac8fe5e36fca2b1c93266
Jared Davis <jadavis@southsidecharter.org>
2020-11-24 22:54:15 -0500

    working out coordinate logic and timestamp

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,25 @@ var cityHumidityEl = document.querySelector("#current-city-hum");
 var cityWindSpeedEl = document.querySelector("#current-city-wind");
 var cityUvIndexEl = document.querySelector("#current-city-uv");
 var forecastDateEl = document.querySelector("#one-date");
+var appId = "221098603a041dd7e688a833d2e946ac"
 
-
-//prevent enter from reloading page
+//get coordinates from city
+function getCoord(cityName) {
+    var coordinates = null;
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=" + appId)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(responseJson) {
+        var cityLat = responseJson.coord.lat;
+        var cityLon = responseJson.coord.lon;
+        coordinates = {
+            latitude: cityLat,
+            longitude: cityLon
+        };
+        return coordinates;
+    })
+}
 
 
 // function to search for a city
@@ -77,28 +93,49 @@ renderCities(list);
 //fetch and push forecast data
 
 function forecastData() {
-    var forecast = JSON.parse(localStorage.getItem("prevCities")) || [];
     var searchTerm = document.getElementById("cityName").value;
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&units=imperial" + "&appid=221098603a041dd7e688a833d2e946ac")
-    .then(function(forecastResponse) {
-        return forecastResponse.json();
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial" + "&appid=" + appId)
+    .then(function(response) {
+        return response.json();
     })
-    .then(function(forecastResponse) {
-        console.log(forecastResponse);
+    .then(function(responseJson) {
+        var cityLat = responseJson.coord.lat;
+        var cityLon = responseJson.coord.lon;
+        coordinates = {
+            latitude: cityLat,
+            longitude: cityLon
+        };
+        return coordinates;
+    })
+    .then(function(coordinates) {
+        fetch("https://api.openweathermap.org/data/2.5/onecall?" + "&lat=" + coordinates.latitude + "&lon=" + coordinates.longitude + "&exclude=minutely,hourly,alerts&appid=" + appId)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(responseJson) {
+            console.log(responseJson);
+        })    
         
-        var date = forecastResponse.list[i].dt_txt;
-        var weatherGraphic = forecastResponse.weather[i].icon;
-        var namedTemp = forecastResponse.main.temp;
-        var namedHumidity = forecastResponse.main.humidity;
+    })
+   
+    
+}
+//converting universal time into usable JavaScript time, multiply by 1000 to get miliseconds for needed date
+function convertUnixTime(timestamp) {
+    return new Date(timestamp*1000).toLocaleDateString();
+}     
+        //var date = forecastResponse.list[i].dt_txt;
+        //var weatherGraphic = forecastResponse.weather[i].icon;
+        //var namedTemp = forecastResponse.main.temp;
+        //var namedHumidity = forecastResponse.main.humidity;
 
-        dateEl.innerHTML = "<h2>" + date + "<img src= http://openweathermap.org/img/wn/" + weatherGraphic + ".png>" + "</h2>";
-        weatherGraphic.innerHTML = "<p>" + "Temperature: " + namedTemp + "&#176;F" + "</p>";
-        namedTemp.innerHTML = "<p>" + "Humidity: " + namedHumidity + "%" + "</p>";
-        namedHumidity.innerHTML = "<p>" + "Wind Speed: " + namedWindSpeed + " MPH" + "</p>";
-    });
-}        
+        //dateEl.innerHTML = "<h2>" + date + "<img src= http://openweathermap.org/img/wn/" + weatherGraphic + ".png>" + "</h2>";
+        //weatherGraphic.innerHTML = "<p>" + "Temperature: " + namedTemp + "&#176;F" + "</p>";
+        //namedTemp.innerHTML = "<p>" + "Humidity: " + namedHumidity + "%" + "</p>";
+        //namedHumidity.innerHTML = "<p>" + "Wind Speed: " + namedWindSpeed + " MPH" + "</p>";
+        
         //iterate over each day
-        for (var i = 0; i < list.length; i++) {
+       /* for (var i = 0; i < list.length; i++) {
             var previousItem = $("<p>");
             previousItem.text(list[i]);
             previousItem.addClass("list-group list-group flush bg-light border text-center")
